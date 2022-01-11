27d3621e6ca98bd87ee4f14d42cb58b4c1f0d8c4
Ishantgarg-web <57170623+Ishantgarg-web@users.noreply.github.com>
2022-01-11 15:08:30 +0530

    Update index.js

--- The Dicee Game/index.js
+++ The Dicee Game/index.js
@@ -1,10 +1,10 @@
 var randomNumber1=Math.floor(Math.random() * 6)+1;
-var leftImage = "images/dice"+randomNumber1+".png";
+var leftImage = "dice"+randomNumber1+".png";
 document.querySelector(".img1").setAttribute("src",leftImage);
 
 
 var randomNumber2=Math.floor(Math.random() * 6)+1;
-var rightImage="images/dice"+randomNumber2+".png";
+var rightImage="dice"+randomNumber2+".png";
 document.querySelector(".img2").setAttribute("src",rightImage);
 
 if(randomNumber1>randomNumber2){
