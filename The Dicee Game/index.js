b9389fdb0d97df8e1b2fb42b89c407e6a91259d4
Ishantgarg-web <57170623+Ishantgarg-web@users.noreply.github.com>
2022-01-11 15:05:46 +0530

    Add files via upload

---
+++ The Dicee Game/index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(Math.random() * 6)+1;
+var leftImage = "images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",leftImage);
+
+
+var randomNumber2=Math.floor(Math.random() * 6)+1;
+var rightImage="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",rightImage);
+
+if(randomNumber1>randomNumber2){
+	document.querySelector("h1").innerHTML="Player 1 wins!";
+}else if(randomNumber2>randomNumber1){
+	document.querySelector("h1").innerHTML="Player 2 wins!";
+}else{
+	document.querySelector("h1").innerHTML="Match is Tie";
+}
